d941528968768acae76df995acb218d09f1af165
Javier Cerón <87388852+Javiec369@users.noreply.github.com>
2024-07-04 13:07:23 -0500

    Update audio_acq.py

--- Laboratorios 2024-2/Lab_SA1/audio_acq.py
+++ Laboratorios 2024-2/Lab_SA1/audio_acq.py
@@ -2,7 +2,7 @@
     Análisis de Señales
     2024-2
     Universidad Tecnológica de Pereira
-    Práctica 1: Muestreo de señales de audio
+    Laboratorio 1: Muestreo de señales de audio
 '''
 
 # Packages
