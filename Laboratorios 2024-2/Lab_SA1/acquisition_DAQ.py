48d3f731ed07ccfeeaa9fbf6c0f3d1f22f262b1b
leydicastaneda11 <131828959+leydicastaneda11@users.noreply.github.com>
2024-07-17 15:41:58 -0500

    save_adquisicion_daq

--- Laboratorios 2024-2/Lab_SA1/acquisition_DAQ.py
+++ Laboratorios 2024-2/Lab_SA1/acquisition_DAQ.py
@@ -1,65 +1,31 @@
-'''
-    Análisis de Señales 
-    2024-2
-    Universidad Tecnológica de Pereira
-    
-    Práctica 1: Adquisición y muestreo de señales
-'''
-
-# Packages
 import nidaqmx
 from nidaqmx.constants import AcquisitionType, READ_ALL_AVAILABLE
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Parámetros de adquisición
 f_daq = 5000  
-f_sam = 10    
-n_bits = 2    
+f_sam = 50    # Frecuencia de muestreo
 
-# Acquisition
-def new_func(f_daq, f_sam):
-    '''
-    Función para adquirir y muestrear una señal.
-    f_daq: Frecuencia de muestreo original
-    f_sam: Frecuencia de muestreo de la señal
+# Adquisición de datos
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("myDAQ1/ai0")
+    task.timing.cfg_samp_clk_timing(f_daq, sample_mode=AcquisitionType.FINITE, samps_per_chan=5000)
+    data = task.read(READ_ALL_AVAILABLE)
 
-    return: data, time, datam, timem
-    '''
-    with nidaqmx.Task() as task:
-        task.ai_channels.add_ai_voltage_chan("myDAQ1/ai0")
-        task.timing.cfg_samp_clk_timing(f_daq, sample_mode=AcquisitionType.FINITE, samps_per_chan=5000)
-        data = task.read(READ_ALL_AVAILABLE)
-    data = np.array(data)
-    time = np.arange(0, len(data) / f_daq, 1 / f_daq)
+data = np.array(data)
+time = np.arange(0, len(data) / f_daq, 1 / f_daq)
 
-    factor = int(f_daq / f_sam)
-    datam = data[::factor]
-    timem = time[::factor]
-    return data,time,datam,timem
+factor = int(f_daq / f_sam)
+datam = data[::factor]
+timem = time[::factor]
 
-data, time, datam, timem = new_func(f_daq, f_sam)
 
-# Digitalization
-nc = (data.max() - data.min()) / (2 ** n_bits)
-quantization_levels = [nc * i for i in range(2 ** n_bits + 1)]
-xd = np.round((data - data.min()) / nc) * nc + data.min()
 
-# Plotting
-fig, axs = plt.subplots(2, 1, figsize=(12, 6))
-
-# Subplot 1
-axs[0].plot(time, data, label='Datos origial', c='blue')
-axs[0].stem(timem, datam, linefmt='r-', markerfmt='ro', basefmt='r-', label='Señal muestreada')
-axs[0].set_ylabel('Amplitud')
-axs[0].set_title('Señal muestreada')
-axs[0].legend(loc='upper right')
-# Subplot 2
-axs[1].plot(time, data, label='Datos origial', c='blue')
-axs[1].step(time, xd, where="post", c="r", label="Señal digital")
-axs[1].set_ylabel('Amplitud')
-axs[1].set_xlabel('Tiempo (s)')
-axs[1].set_title('Señal digitalizada')
-axs[1].legend(loc='upper right')
-
+# Graficar la señal original
+plt.plot(time, data, label='Datos original')
+plt.stem(timem, datam, linefmt='r-', markerfmt='ro', basefmt='r-', label='Señal muestreada')
+plt.ylabel('Amplitud')
+plt.xlabel('Tiempo (s)')
+plt.title('Señal muestreada y digitalizada')
+plt.legend()
 plt.show()
