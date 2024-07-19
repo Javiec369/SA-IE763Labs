947554bd4a350d5e044d9c77247dbcf9608b32a2
Javier Cerón <javier.ceron@utp.edu.co>
2024-07-19 12:05:15 -0500

    Acquisition file updated

--- Laboratorios 2024-2/Lab_SA1/acquisition_DAQ.py
+++ Laboratorios 2024-2/Lab_SA1/acquisition_DAQ.py
@@ -14,12 +14,12 @@ import numpy as np
 # Parámetros de adquisición
 f_daq = 5000  
 f_sam = 10    
- 
-# Acquisition
-def new_func(f_daq, f_sam):
-    '''Función para adquirir y muestrear una señal.
 
-    f_daq: Frecuencia de muestro de la DAQ  
+# Adquisición y muestreo de señales
+def new_func(f_daq, f_sam):
+    '''
+    Función para adquirir y muestrear una señal.
+    f_daq: Frecuencia de muestreo original
     f_sam: Frecuencia de muestreo de la señal
 
     return: data, time, datam, timem
@@ -30,40 +30,41 @@ def new_func(f_daq, f_sam):
         data = task.read(READ_ALL_AVAILABLE)
     data = np.array(data)
     time = np.arange(0, len(data) / f_daq, 1 / f_daq)
-
     factor = int(f_daq / f_sam)
     datam = data[::factor]
     timem = time[::factor]
-    return data,time,datam,timem
 
+    return data, time, datam, timem
+
+# Subplot 1: Señal original y muestreada
+plt.figure(figsize=[10, 4])
 data, time, datam, timem = new_func(f_daq, f_sam)
+plt.subplot(111)
+plt.plot(time, data, label='Datos original', c='blue')
+plt.stem(timem, datam, linefmt='r-', markerfmt='ro', basefmt='r-', label='Señal muestreada')
+plt.xlabel('Tiempo (s)')
+plt.legend(loc='upper right')
+plt.title('Señal original y muestreada')
+plt.show()
 
-# Digitalización 
-'''Realizar el código para la digitalización de la señal 
-muestreada con n_bits de cuantización. Modificar apartir de 
-la línea siguiente a la variable declarada 'n_bits = 2'
-'''
-n_bits = 2
-####Código------------------------------------------------- 
-digital_signal = np.zeros(len(datam))
+# Subplot 2: Primer aliasing
+plt.figure(figsize=[10, 4])
+data_alias1, time_alias1, datam_alias1, timem_alias1 = new_func(f_daq, f_sam)
+plt.subplot(111)
+plt.plot(time_alias1, data_alias1, label='Datos origial', c='blue')
+plt.stem(timem_alias1, datam_alias1, linefmt='r-', markerfmt='ro', basefmt='r-', label='Señal muestreada')
+plt.xlabel('Tiempo (s)')
+plt.legend(loc='upper right')
+plt.title('Primer aliasing')
+plt.show()
 
-####-------------------------------------------------------
-
-# Gráficas
-fig, axs = plt.subplots(2, 1, figsize=(12, 6))
-
-# Subplot 1
-axs[0].plot(time, data, label='Datos origial', c='blue')
-axs[0].stem(timem, datam, linefmt='r-', markerfmt='ro', basefmt='r-', label='Señal muestreada')
-axs[0].set_ylabel('Amplitud')
-axs[0].set_title('Señal muestreada')
-axs[0].legend(loc='upper right')
-# Subplot 2
-axs[1].plot(time, data, label='Datos origial', c='blue')
-axs[1].step(time, digital_signal, where="post", c="r", label="Señal digital")
-axs[1].set_ylabel('Amplitud')
-axs[1].set_xlabel('Tiempo (s)')
-axs[1].set_title('Señal digitalizada')
-axs[1].legend(loc='upper right')
-
+# Subplot 3: Segundo aliasing
+plt.figure(figsize=[10, 4])
+data_alias2, time_alias2, datam_alias2, timem_alias2 = new_func(f_daq, f_sam)
+plt.subplot(111)
+plt.plot(time_alias2, data_alias2, label='Datos original', c='blue')
+plt.stem(timem_alias2, datam_alias2, linefmt='r-', markerfmt='ro', basefmt='r-', label='Señal muestreada')
+plt.xlabel('Tiempo (s)')
+plt.legend(loc='upper right')
+plt.title('Segundo aliasing')
 plt.show()
