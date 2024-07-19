277a03baa9fcdf3f525606426e251cccdf34b8fd
Javier Cerón <javier.ceron@utp.edu.co>
2024-07-19 15:19:56 -0500

    updated audio_acq file

--- Laboratorios 2024-2/Lab_SA1/audio_acq.py
+++ Laboratorios 2024-2/Lab_SA1/audio_acq.py
@@ -2,77 +2,61 @@
     Análisis de Señales
     2024-2
     Universidad Tecnológica de Pereira
-    Laboratorio 1: Muestreo de señales de audio
+
+    Práctica 1: Muestreo de señales de audio
 '''
 
 # Packages
 from pydub import AudioSegment
-import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq, rfft, rfftfreq
 from scipy.io import wavfile
+import io
 
 # Frecuencias de muestreo: [1000, 2000, 3000] Hz
-new_sample_rate = 2000
+new_sample_rate = 650
 
-# Se requiere el path del archivo de audio
 # Dar click derecho en el archivo de audio y seleccionar "Copy Relative Path".
-audio_path = r"Lab1_2024-2\\piano_music.mp3"
+audio_path = "C:\\Users\\Admin\\Desktop\\Signals\\LabsSA_Local\\Lab1\\Lab1_local\\piano_music.wav"
+audio_original = AudioSegment.from_wav(audio_path)
 
-audio = AudioSegment.from_mp3(audio_path)
-d = audio.export("audio.wav", format="wav")
+# Exportar el audio original a un buffer en memoria
+buffer = io.BytesIO()
+audio_original.export(buffer, format="wav")
+buffer.seek(0)
 
-def upload_and_resample_audio(new_sample_rate):
-    ''' Función para cargar un archivo de audio
-    '''
-    resampled_audio = audio.set_frame_rate(new_sample_rate)
-    # Asignar un nombre al archivo temporal que crearemos
-    # ruta: nombre_carpeta\\temp_audio.wav
-    temp_file = r"ruta"
-    resampled_audio.export(temp_file, format="wav")
-upload_and_resample_audio(new_sample_rate)
+# Leer el buffer con scipy.io.wavfile.read
+sample_rate, audio_data = wavfile.read(buffer)
+audio_data = audio_data[:, 1]
 
-#Aplicar la fft de scipy al archivo de audio original
-sample_rate, audio_data1 = wavfile.read(d)
-audio_data1 = audio_data1[:, 1]
-'''Realizar el código para aplicar la fft a la señal de audio 'audio_data1'.
-'''
-####Código-------------------------------------------------
+# Transformada de Fourier datos originales
+x1 = np.fft.rfft(audio_data)
+f1 = np.fft.rfftfreq(len(audio_data), d=1/sample_rate)
 
-frecuencia_original = np.zeros(len(audio_data1))
-####-------------------------------------------------------
+# Muestreo de la señal de audio
+sample = audio_original.set_frame_rate(new_sample_rate)
+audio_muestreado = sample.export("temp_sample.wav", format="wav")
 
-
-#Aplicar la fft de scipy al archivo de audio muestreado
-audio2 = AudioSegment.from_wav(r"ruta")
-s=audio2.export("temp2.wav", format="wav")
-
-sample_rate2, audio_data2 = wavfile.read(s)
+sample_rate2, audio_data2 = wavfile.read(audio_muestreado)
 audio_data2 = audio_data2[:, 1]
-'''Realizar el código para aplicar la fft a la señal de audio 'audio_data2'.
-'''
-####Código-------------------------------------------------
 
-frecuencia_sample = np.zeros(len(audio_data2))
-####-------------------------------------------------------
+# Transformada de Fourier datos muestreados
+x2 = np.fft.rfft(audio_data2)
+f2 = np.fft.rfftfreq(len(audio_data2), d=1/sample_rate2)
 
 # Gráficas
-plt.figure(figsize=(12, 6))
+plt.figure(figsize=(10, 5))
 
-# Una vez obtenga las fft de las señales de audio, grafique
-# el espectro de frecuencias de ambas señales de audio.
-plt.subplot(2, 1, 1)
-plt.plot(frecuencia_original, np.abs(x_original)**2)
+plt.subplot(211)
+plt.plot(f1, np.abs(x1)**2)
 plt.title('Espectro de Frecuencias (Audio Original)')
-plt.xlim(0, new_sample_rate)
+plt.xlim(0, 3000)
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Amplitud')
 
-plt.subplot(2, 1, 2)
-plt.plot(frecuencia_sample, np.abs(x_sample)**2)
+plt.subplot(212)
+plt.plot(f2, np.abs(x2)**2)
 plt.title('Espectro de Frecuencias (Audio Muestreado)')
-plt.xlim(0, new_sample_rate/2)
 plt.xlabel('Frecuencia (Hz)')
 plt.ylabel('Amplitud')
 
